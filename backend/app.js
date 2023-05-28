5c7e909512043bdb4b6f348c34f48c520aaa898d
Sanjeevi1412 <112859281+Sanjeevi1412@users.noreply.github.com>
2023-05-29 00:30:13 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -4,22 +4,26 @@ const app = express();
 
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const fileUpload = require("express-fileupload");
 
+const cors = require("cors");
 
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(fileUpload({useTempFiles: true}));
-
-
+app.use(cors());
+app.use("/",express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 if(process.env.NODE_ENV !== "PRODUCTION"){
     require("dotenv").config({
         path:"backend/config/.env"
-    })
+    });
 }
+
+const user =require("./controller/user");
+
+app.use("/api/v2/user", user);
+
 
 app.use(ErrorHandler)
 
