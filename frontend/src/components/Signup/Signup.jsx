24dadf07d8aee81ebd13a40244b93529c424f2ef
Aravinthansparrow <aravinthan.istardevelopers@gmail.com>
2023-05-28 16:31:40 +0530

    signup

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -2,48 +2,58 @@ import { React, useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/styles";
 import { Link, useNavigate } from "react-router-dom";
+import {RxAvatar} from  "react-icons/rx"
+
 // import axios from "axios";
 // import { server } from "../../server";
 // import { toast } from "react-toastify";
 
-const Login = () => {
+const Signup = () => {
 
     const navigate = useNavigate();
     const [email, setEmail] = useState("");
+    const [name ,setName] = useState("");
     const [password, setPassword] = useState("");
     const [visible, setVisible] = useState(false);
+    const [avatar,setAvatar] = useState(null)
   
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-    //   await axios
-    //     .post(
-    //       `${server}/user/login-user`,
-    //       {
-    //         email,
-    //         password,
-    //       },
-    //       { withCredentials: true }
-    //     )
-    //     .then((res) => {
-    //       toast.success("Login Success!");
-    //       navigate("/");
-    //       window.location.reload(true); 
-    //     })
-    //     .catch((err) => {
-    //       toast.error(err.response.data.message);
-    //     });
+    const handleSubmit = () => {
+      console.log("Success");
+    
     };
+    const handleInputChang= (e) => {
+      const file = e.target.files[0];
+      setAvatar(file)
+    }
   return (
         <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-md">
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Login to your account
+              Register as a new user
             </h2>
           </div>
           <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
               <form className="space-y-6" onSubmit={handleSubmit}>
+                <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Full Name
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        name="text"
+                        autoComplete="name"
+                        required
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                      />
+                    </div>
+                </div>
                 <div>
                   <label
                     htmlFor="email"
@@ -120,6 +130,25 @@ const Login = () => {
                   </div>
                 </div>
                 <div>
+                  <label htmlFor="avatar"
+                  className="block text-sm font-medium text-gray-700"></label>
+                  <div className="mt-2 flex items-center">
+                  <span className="inline-block h-8 w-8 rounded-full overflow-hidden">
+                    {
+                      avatar ? ( <img src={URL.createObjectURL(avatar)} alt="avator" className="h-full w-full object-cover rounded-full"/>)
+                      : (<RxAvatar className="h-8 w-8" />)
+                    }
+                  </span>
+                  
+                    <label htmlFor="file-input" className="ml-5 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
+                      <span>Upload a file</span>
+                      <input type="file" name="avatar" id="file-input" accept=".jpg,.jpeg,.png"
+                      onChange={handleInputChang}
+                      className="sr-only" />
+                    </label>
+                    </div>
+                </div>
+                <div>
                   <button
                     type="submit"
                     className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
@@ -129,8 +158,8 @@ const Login = () => {
                 </div>
                 <div className={`${styles.noramlFlex} w-full`}>
                   <h4>Not have any account?</h4>
-                  <Link to="/sign-up" className="text-blue-600 pl-2">
-                    Sign Up
+                  <Link to="/" className="text-blue-600 pl-2">
+                    Sign In
                   </Link>
                 </div>
               </form>
@@ -140,4 +169,4 @@ const Login = () => {
       );
 }
 
-export default Login
+export default Signup
