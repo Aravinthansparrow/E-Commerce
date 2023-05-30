04cfb45bb8b581a1db2c6f2ace61603dc1f333cf
Aravinthansparrow <aravinthan.istardevelopers@gmail.com>
2023-05-30 20:48:57 +0530

    new changes error cleared

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from './reportWebVitals';
-import Store from './redux/store'
+import {Provider} from 'react-redux';
+import Store from './redux/store';
 
 ReactDOM.render(
   <Provider store={Store}>
